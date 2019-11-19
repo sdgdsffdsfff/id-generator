fd0c937a31c8c858214e9ef5643ee533b87052d5
wen_jf <wen_jf@suixingpay.com>
2019-11-19 15:45:32 +0800

    commit

---
+++ src/main/java/com/wenjifeng/idgenerator/config/Test1.java
@@ -0,0 +1,11 @@
+package com.wenjifeng.idgenerator.config;
+
+/**
+ * @Description TODO
+ * @className Test1
+ * @Author wen_jf@suixingpay.com
+ * @Date 2019/11/19
+ * @Version 1.0
+ **/
+public class Test1 {
+}
